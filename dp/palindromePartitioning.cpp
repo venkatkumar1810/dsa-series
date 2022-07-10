0cbaefc18d3903825d93389829fc96b050d6e136
venkatkumar1810 <67142458+venkatkumar1810@users.noreply.github.com>
2022-07-10 13:43:43 +0530

    dp

---
+++ dp/palindromePartitioning.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+bool isPalindrome(int i, int j, string &temp){
+    while(i <= j){
+        if(temp[i] != temp[j]) return false;
+        i++;
+        j--;
+    }
+    return true;
+}
+int f(int i, int n, string &str, vector<int> &dp){
+    if(i == n) return 0;
+    if(dp[i] != -1) return dp[i];
+    int minCost = INT_MAX;
+    
+    for(int j = i; j < n; j++){
+        if(isPalindrome(i, j, str)){
+            int cost = 1 + f(j + 1, n, str, dp);
+            minCost = min(minCost, cost);
+        }
+    }
+    return dp[i] = minCost;
+}
+int palindromePartitioning(string str) {
+    // Write your code here
+    
+    int n = str.size();
+//     vector<int> dp(n, -1);
+//     return f(0, n, str, dp) - 1;
+    vector<int> dp(n+1, 0);
+    dp[n] = 0;
+    for(int i = n - 1; i>= 0; i--){
+        int minCost = INT_MAX;
+        for(int j = i; j<n; j++){
+            if(isPalindrome(i, j, str)){
+                int cost = 1 + dp[j + 1];
+                minCost = min(minCost, cost);
+            }
+        }
+        dp[i] = minCost;
+    }
+    return dp[0] - 1;
+}
