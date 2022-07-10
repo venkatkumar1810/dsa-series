01c7b083e79b31e019b734faa3ee297e9a1145f3
venkatkumar1810 <67142458+venkatkumar1810@users.noreply.github.com>
2022-07-10 16:57:57 +0530

    dp

---
+++ dp/wordBreak.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool> dp(s.size() + 1, false);
+        dp[s.size()] = true;
+        
+        for(int i = s.size() -1; i>=0; i--){
+            for(auto &w: wordDict){
+                if( i + w.size() <= s.size()
+                  and s.substr(i, w.size()) == w){
+                    dp[i] = dp[i + w.size()];
+                }
+                if(dp[i]) break;
+            }
+        }
+        return dp[0];
+    }
+};
