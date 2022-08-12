ab33f3a01f8ce89d14078e7e4ef28fe0750d6186
venkatkumar1810 <67142458+venkatkumar1810@users.noreply.github.com>
2022-08-12 09:47:59 +0530

    part dp

---
+++ dp/partitionArrayForMaximumSub.cpp
@@ -0,0 +1,22 @@
+int f(int ind, vector<int> &num, int k, vector<int> &dp){
+    if(ind == num.size()) return 0;
+    if(dp[ind] != -1) return dp[ind];
+    int len = 0; 
+    int maxi = INT_MIN;
+    int maxAns = INT_MIN;
+    int n = num.size();
+    for(int j = ind; j < min(n, ind + k); j++){
+        len++;
+        maxi = max(maxi, num[j]);
+        int sum = len*maxi + f(j + 1, num, k, dp);
+        maxAns = max(maxAns, sum);
+    }
+    return dp[ind] = maxAns;
+}
+int maximumSubarray(vector<int> &num, int x)
+{
+    // Write your code here.
+    int n = num.size();
+    vector<int> dp(n , -1);
+    return f(0, num, x, dp);
+}
